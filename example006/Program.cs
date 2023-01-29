fba5a67a5c2d82c401473512f2c8f35589dcb89c
ixplodd <egorsgor01@gmail.com>
2023-01-29 16:45:35 +0300

    задача с гирями

---
+++ example006/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 3;
+int b = 8;
+int c = 1;
+int d = 2;
+int e = 6;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+
